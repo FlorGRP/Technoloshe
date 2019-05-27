56da2dc0ff4b0a5242ab04e9e4ff56abad653c00
Florencia Rodriguez Perez <florencia.rodriguezperez@gmail.com>
2019-05-27 19:39:39 -0300

    nodos

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
    Array : []
    Function
    
- VARIABLES: var  -  let - const 
+ VARIABLES: var - let - const 
   Los nombres de una variable pueden arrancar con _ $ o cualquier letra
 
  */
@@ -56,6 +56,7 @@
  /*console.log(numeros)
  console.log(numeros[5])
  console.log(numeros.length)*/
+ 
  console.table(numeros)
 
  //Estructuras de control: Condicionales-Bucles/Ciclos
@@ -71,3 +72,100 @@ for (let prop in persona) {
     
       console.log(prop)  
     }
+
+//V = JS Engine
+//WEB APIs
+//Objeto Global = window =BOM =Browser Object Model
+
+//var a = 1
+//window.a = 1
+
+//window.innerHeight    innerHeight :El alto en pixeles
+//window.Width          innerWidth  :El ancho en pixeles
+
+
+//outerHeight =>Number - El alto en PX del navegador
+//outerWidth => Number - El ancho en PX del navegador
+
+//console => Object - Nos da funciones de logueo en consola de desarrollo
+//location => Object - Tiene propiedades que controlan la ubicacion de un usuario
+//location.href = String - Mapea a la barra de direcciones
+//history.back() = Retrocede un nivel en el historial
+//history.forward() = Avanza un nivel de en el historial
+
+//document = DOM = Document Object Model
+
+//console.dir(X) = Muestra X en formato JSON
+//JSON= JavaScript Object Notation
+//console.clear() = Limpia la consola
+//console.dir(document)
+
+
+//SELECCIONAR NODOS 
+//document.getElementById(X) = DomElement
+//document.getElementsByClassName(X) Array
+//document.getElementsByTagName(X) Array
+//document.querySelector(X) DomElement - Busca la primera instancia del selector X
+//document.querySeletor All(X) Array - Busca todas las instancias del selector X
+
+
+/*let dos = document.getElementById("item2")
+console.dir(dos)
+dos.innerText = "Nuevo Item"
+
+let items =document.getElementsByClassName("item")
+let lis = document.getElementsByTagName("li")*/
+
+
+let dos = document.querySelector("#item2")
+
+
+let items =document.querySelectorAll(".item")
+
+
+let lis= document.querySelectorAll("li")
+
+
+//CREAR NODOS
+let p = document.createElement("p")
+p.innerText = "Mi nuevo Texto"
+p.id ="nuevo"
+p.className = "item"
+
+//AGREGAR NODOS AL DOM
+//Nodo.appendChild(NodoNuevo) => Inserta el NodoNuevo como ultimo hijo de Nodo
+//Nodo.insertBefore(NodoNuevo,NodoRed) => Inserta el NodoNuevo antes de NodoRef
+
+let body =document.querySelector("body")
+body.appendChild(p)
+
+/*
+1-Crear un elemento <li> y ponerle un texto
+2-Meterlo como primer item del <ul>
+
+EXTRA
+1-Crear un elemento <ul> nuevo y vacio
+let nuevo_ul= document.createelement("ul)
+2-Hacer un bucle de 5 iteraciones
+for(let i=0; i<5; i++)
+3-Por iteracion de ciclo se tiene que crear un <li> nuevo
+let li_nuevo=document.createelement("li")
+li_nuevo.innerText = "Nuevo Texto" + i
+4-Agregar todos los <li> al <ul> del punto 1
+nuevo_ul.appendChild(li_nuevo)
+5- Agregar el <ul> del punto 1 al DOM 
+body.appendchild(nuevo_ul)
+
+*/
+
+
+let linuevo = document.createElement("li")
+linuevo.innerText = "Hola a todos"
+
+let ul = document.querySelector("ul")
+let li1= document.querySelector("#item1")
+ul.insertBefore(linuevo,li1)
+
+
+
+
